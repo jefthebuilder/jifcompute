4a60050d68d85f7de3839fc3a06bdb20d9f79cb0
jefthebuilder <162872160+jefthebuilder@users.noreply.github.com>
2024-05-30 09:46:11 +0200

    Update cpu.v

--- src/cpu.v
+++ src/cpu.v
@@ -14,49 +14,23 @@ module cpu(
     reg [2:0] state;
     reg [31:0] addr;
     reg [31:0] instr;
-    reg [31:0] rega;
-    reg [31:0] regb;
-    reg [31:0] regc;
-    reg [15:0] value;
-    reg flag1;
-    reg flag2;
-    reg flag3;
+    wire [31:0] rega;
+    wire [31:0] regb;
+    wire [31:0] regc;
+    wire [15:0] value;
+    wire flag1;
+    wire flag2;
+    wire flag3;
     reg highlow;
     reg [31:0]temp_address;
     reg [31:0] naddr;
     wire temp2;
     wire writinginstr;
-    always @(posedge reset)
-    begin
-        {a,b,c,d,e,f,g,h} = 0;
-        {fa,fb,fc,fd,fe,ff,fg,fh} = 0;
-        {addr,instr} = 0;
-
-        assign state = 0;
-        assign addrchange = 0;
-    end
-    ADDER32 adder1(addr,1,temp_address);
-    ALU alu1(clock,rega,regb,h,value,highlow,flag1,flag2,flag3,instr[6:0],regc,addrchange,naddr);
-    always@(posedge clock)
-        begin
-
-
-
-                case (state)
-                    0: begin
-                        address = addr;
-                        rw = 0;
-                        instr = data;
-                        state = 1;
-                    end
-                    1: begin
-                        // regA
-
-                        case (instr[8:5])
+    case (instr[8:5])
                             0: assign rega = a;
                             1: assign rega = b;
                             2: assign  rega = c;
-                            3:  assign rega = d;
+                            3: assign rega = d;
                             4: assign rega = e;
                             5: assign rega = f;
                             6: assign rega = g;
@@ -94,35 +68,62 @@ module cpu(
                         endcase
                         assign value = instr[30:15];
                         assign highlow = instr[15:14];
+    case (instr[14:11])
+                            0: assign fa = flag3;
+                            1: assign fb = flag3;
+                            2: assign fc = flag3;
+                            3: assign fd = flag3;
+                            4: assign fe = flag3;
+                            5: assign ff = flag3;
+                            6: assign fg = flag3;
+                            7: assign fh = flag3;
+                        endcase
+                        case (instr[14:11])
+                            0: assign fa = flag3;
+                            1: assign fb = flag3;
+                            2: assign fc = flag3;
+                            3: assign fd = flag3;
+                            4: assign fe = flag3;
+                            5: assign ff = flag3;
+                            6: assign fg = flag3;
+                            7: assign fh = flag3;
+                        endcase
 
-                        assign temp_address = addr;
-                        assign temp2 = ~addrchange;
+    always @(posedge reset)
+    begin
+        {a,b,c,d,e,f,g,h} = 0;
+        {fa,fb,fc,fd,fe,ff,fg,fh} = 0;
+        {addr,instr} = 0;
+
+        state = 0;
+        addrchange = 0;
+    end
+    ADDER32 adder1(addr,1,temp_address);
+    ALU alu1(clock,rega,regb,h,value,highlow,flag1,flag2,flag3,instr[6:0],regc,addrchange,naddr);
+    always@(posedge clock)
+        begin
+
+
+
+                case (state)
+                    0: begin
+                        address = addr;
+                        rw = 0;
+                        instr = data;
+                        state = 1;
+                    end
+                    1: begin
+                        // regA
+
+                        
+                        temp_address = addr;
+                        temp2 = ~addrchange;
                         addr = (temp2 & temp_address) | naddr;
                         writinginstr = instr[6:0] == 7;
                         address = (h & writinginstr);
                         rw = writinginstr;
                         datao = rega;
-                        case (instr[14:11])
-                            0: assign fa = flag3;
-                            1: assign fb = flag3;
-                            2: assign fc = flag3;
-                            3: assign fd = flag3;
-                            4: assign fe = flag3;
-                            5: assign ff = flag3;
-                            6: assign fg = flag3;
-                            7: assign fh = flag3;
-                        endcase
-                        case (instr[14:11])
-                            0: assign fa = flag3;
-                            1: assign fb = flag3;
-                            2: assign fc = flag3;
-                            3: assign fd = flag3;
-                            4: assign fe = flag3;
-                            5: assign ff = flag3;
-                            6: assign fg = flag3;
-                            7: assign fh = flag3;
-                        endcase
-
+                        
                     end
                     2:
                         begin
