de0ba892bebe9665ded3de8800c81e1af969e05e
jefthebuilder <jefgamecoffe@gmail.com>
2024-05-30 20:48:06 +0200

    update files

--- src/cpu.v
+++ src/cpu.v
@@ -199,7 +199,7 @@ module cpu(
   
    
      assign address = ((addro & {32{stato == 0 & clock}}) | (h & writinginstr));
-     assign rw = (~(~(stato == 0 ) | ~writinginstr)) & ( clock);
+     assign rw = (state == 0) | ((state != 0) & writinginstr != 7);
      assign state = ((({2{stato == 0}} & 1) | ({2{stato == 1}} & 2) ) | ({2{stato == 2}} & 0));
      assign wstate = 1;
     // state 1 removed temp change
