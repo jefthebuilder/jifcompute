bbc2e2c54e28332084a9d32473ff3b7f4b27a2f9
jefthebuilder <162872160+jefthebuilder@users.noreply.github.com>
2024-05-30 16:01:27 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -10,23 +10,22 @@ module tt_um_jefloverockets_cpuhandler (
     output reg [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output reg [7:0] uio_out,  // IOs: Output path
-    output reg uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    output reg [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-
+  wire rw;
   wire rst;
   not(rst,rst_n);
   reg [4:0] count;
   reg [31:0] data;
   reg [31:0] dataout;
   reg [31:0] addr;
-  
-  
-  cpu cpf(data,addr,dataout,uio_oe,clk,rst);
+  assign uio_oe = {8{rw}};
+  cpu cpf(data,addr,dataout,rw,clk,rst);
   always@(posedge clk)
           begin
                case( count)
