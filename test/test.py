3afd75b7a0c90ceb485775c9893d5b12dd70d515
jefthebuilder <jefgamecoffe@gmail.com>
2024-05-31 21:32:01 +0200

    update files

--- test/test.py
+++ test/test.py
@@ -45,7 +45,7 @@ async def testprogram(dut,program,result=158+158,maxi=100):
 
     i = 0
     while not done and i < maxi:
-        await ClockCycles(dut.clk,2)
+        await ClockCycles(dut.clk,1)
         dut._log.info("state:" + str(dut.uo_out) +" " + str(dut.uio_in) + " "+  str(dut.uio_out))
         data,addr = await read(dut)
         await ClockCycles(dut.clk,1)
@@ -83,7 +83,7 @@ async def test_project(dut):
         await ClockCycles(dut.clk, 1)
         dut._log.info("state:" + str(dut.uo_out) +" " + str(dut.uio_in) + " "+  str(dut.uio_out))
     dut.rst_n.value = 1
-    for i in range(2):
+    for i in range(1):
 
         await ClockCycles(dut.clk, 1)
         dut._log.info("state:" + str(dut.uo_out) +" " + str(dut.uio_in) + " "+  str(dut.uio_out))
