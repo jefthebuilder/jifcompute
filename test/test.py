c51664d3c97d43291b02d7f8e203fb21835852d9
jefthebuilder <jefgamecoffe@gmail.com>
2024-05-31 19:41:08 +0200

    update files

--- test/test.py
+++ test/test.py
@@ -68,10 +68,11 @@ async def test_project(dut):
         await ClockCycles(dut.clk, 1)
         dut._log.info("state:" + str(dut.uo_out) +" " + str(dut.uio_in) + " "+  str(dut.uio_out))
     dut.rst_n.value = 1
-    for i in range(1):
+    for i in range(2):
 
         await ClockCycles(dut.clk, 1)
         dut._log.info("state:" + str(dut.uo_out) +" " + str(dut.uio_in) + " "+  str(dut.uio_out))
+
     print()
 
 
