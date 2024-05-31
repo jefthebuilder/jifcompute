79b47805c51d9509fe44acf3ec0756e31243f9f0
jefthebuilder <162872160+jefthebuilder@users.noreply.github.com>
2024-05-31 10:24:15 +0200

    Update alu.v

--- src/alu.v
+++ src/alu.v
@@ -67,14 +67,14 @@ module ADDER32(input1,input2,answer);
     input [N-1:0] input1,input2;
     output [N-1:0] answer;
     wire  carry_out;
-    wire [N-1:0] carry;
+    wire [N-1:-1] carry;
     genvar i;
     generate
         for(i=0;i<N;i=i+1)
-            begin: generate_N_bit_Adder
-                if(i==0)
-                    half_adder gen_half(input1[0],input2[0],answer[0],carry[0]);
-                    else
+            begin: 
+                
+                    
+                    
                     full_adder gen_full(input1[i],input2[i],carry[i-1],answer[i],carry[i]);
             end
         assign carry_out = carry[N-1];
