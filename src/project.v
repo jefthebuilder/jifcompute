20aaaa42af9db2f004dbdac899af2e5093b46c07
jefthebuilder <162872160+jefthebuilder@users.noreply.github.com>
2024-05-31 12:23:04 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,59 +21,64 @@ module tt_um_jefloverockets_cpuhandler (
   wire rw;
   wire rst;
   not(rst,rst_n);
-  reg [4:0] count;
+  wire [4:0] count;
+  wire wcount;
+  
   reg [31:0] data;
   reg [31:0] dataout;
   reg [31:0] addr;
   assign uio_oe = {8{rw}};
   assign cpuclock = count == 0;
   cpu cpf(data,dataout,addr,rw,cpuclock,rst);
+  wire tcount <= count == 0 & 1 | count == 1 & 2 | count == 3 & 4 | count == 4 & 5 | count == 6 & 7 | count == 7 & 8 | count == 8 & 0;
+  assign wcount <= posedge clock;
+  register regcount(clk,wcount,rst,tcount,count);
   always@(posedge clk)
           begin
                case( count)
                   0: begin
 
-                  count <= 1;
+                  
                   end
                   1: begin
                       uo_out <= addr[7:0];
                       uio_out <= dataout[7:0];
-                     count <= 2;
+                     
                   end
                   2: begin
                       uo_out <= addr[15:8];
                       uio_out <= dataout[15:8];
-                     count <= 3;
+                     
                   end
                   3: begin
                    uo_out <= addr[23:16];
                    uio_out <= dataout[23:16];
-                  count <= 4;
+                  
                   end
                   4: begin
                    uo_out <= addr[31:24];
                    uio_out <= dataout[31:24];
-                  count <= 5;
+                  
                   end
                   5: begin
                   data[7:0] <= uio_in;
-                  count <= 6;
+                  
                   end
                   6: begin
                   data[15:8] <= uio_in;
 
-                  count <= 7;
+                  
                   end
                   7: begin
                   data[23:16] <= uio_in;
 
-                  count <= 8;
+                  
                   end
                   8: begin
                   data[31:24] <= uio_in;
                   count <= 0;
                   end
-                  default: count <= 0;
+                  
                endcase
 
 
