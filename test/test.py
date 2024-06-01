5a51cd22eca3bb8b381a82ce31428ce8748305f3
jefthebuilder <162872160+jefthebuilder@users.noreply.github.com>
2024-06-01 07:31:56 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -99,7 +99,7 @@ async def test_project(dut):
         await ClockCycles(dut.clk, 1)
         dut._log.info("state:" + str(dut.uo_out) +" " + str(dut.uio_in) + " "+  str(dut.uio_out))
     dut.rst_n.value = 1
-    for i in range(30):
+    for i in range(27):
 
         await ClockCycles(dut.clk, 1)
         dut._log.info("state:" + str(dut.uo_out) +" " + str(dut.uio_in) + " "+  str(dut.uio_out))
