b2df7e45cf46b7918390a3276235c719a0317e34
jefthebuilder <jefgamecoffe@gmail.com>
2024-06-01 20:10:13 +0200

    update files

--- src/project.v
+++ src/project.v
@@ -47,7 +47,7 @@ module tt_um_jefloverockets_cpuhandler (
   
   assign tcount = {5{(count <= 8)}} & count + 1;
 
-  always@(posedge clk)
+  always@(clk)
           begin
 
                case( count)
