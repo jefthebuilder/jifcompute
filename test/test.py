51972d8d64ce94d15c7221ced79b09aadf8d66bd
jefthebuilder <jefgamecoffe@gmail.com>
2024-06-01 16:27:23 +0200

    update files

--- test/test.py
+++ test/test.py
@@ -96,7 +96,7 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    for i in range(30):
+    for i in range(31):
 
         await ClockCycles(dut.clk, 1)
         dut._log.info("state:" + str(dut.uo_out) +" " + str(dut.uio_in) + " "+  str(dut.uio_out))
