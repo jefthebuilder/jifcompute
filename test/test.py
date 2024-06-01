d8757488276518422e43fb0db5bc2c69f23b68f8
jefthebuilder <jefgamecoffe@gmail.com>
2024-06-01 08:10:27 +0200

    update files

--- test/test.py
+++ test/test.py
@@ -16,7 +16,7 @@ program_add = [
 async def write(dut, value):
     global cycles
     value = value.replace("_","")
-    for byte in reversed(range(0,32,8)):
+    for byte in range(0,32,8):
         dut._log.info("writing")
 
         dut.uio_in.value = int(value[byte:byte+8],2)
@@ -61,6 +61,7 @@ async def testprogram(dut,program,result=158+158,maxi=100):
         data,addr = await read(dut)
         # await ClockCycles(dut.clk,1)
         # cycles+=1
+        dut._log.info("state:" + str(dut.uo_out) +" " + str(dut.uio_in) + " "+  str(dut.uio_out))
         readwrite = dut.uo_out[0]
         dut._log.info(str(data) + " addr: " + str(addr))
         if addr >= len(program):
