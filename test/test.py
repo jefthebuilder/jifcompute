4abcd6135256cee945fcc9fb9f0dc1804b171d2d
jefthebuilder <jefgamecoffe@gmail.com>
2024-06-01 10:26:55 +0200

    update files

--- test/test.py
+++ test/test.py
@@ -63,7 +63,7 @@ async def testprogram(dut,program,result=158+158,maxi=100):
         # cycles+=1
         dut._log.info("state:" + str(dut.uo_out) +" " + str(dut.uio_in) + " "+  str(dut.uio_out))
         readwrite = dut.uo_out[0]
-        dut._log.info(str(data) + " addr: " + str(addr))
+        dut._log.info(str(data) + " addr: " + str(addr) + "rw:" + str(readwrite))
         if addr >= len(program):
             assert False
 
