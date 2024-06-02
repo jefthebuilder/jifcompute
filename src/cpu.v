ca987b4d1a8163e1f0a96c47bf357d9674a311d9
jefthebuilder <jefgamecoffe@gmail.com>
2024-06-02 21:37:36 +0200

    update files

--- src/cpu.v
+++ src/cpu.v
@@ -23,17 +23,14 @@ input             clear;
 input      [31:0] data_in;
 output reg [31:0] data_out;
 
-always @(posedge clock)
+always @(posedge clock or posedge clear)
 begin
     if(r_enable)
         data_out <= data_in;
-
-
+    if (clear)
+        data_out <= 0;
 end
-always @(posedge clear)
-begin
-data_out <= 0;
-end
+
 endmodule
 
 
@@ -45,17 +42,14 @@ input             clear;
 input      [4:0] data_in;
 output reg [4:0] data_out;
 
-always @(posedge clock)
+always @(posedge clock or posedge clear)
 begin
     if(r_enable)
         data_out <= data_in;
     if (clear)
         data_out <= 0;
 end
-always @(posedge clear)
-begin
-data_out <= 0;
-end
+
 endmodule
 
 module State(clock, r_enable, clear, data_in, data_out);
@@ -66,17 +60,14 @@ input             clear;
 input    [2:0]         data_in;
 output reg   [2:0]     data_out;
 
-always @(posedge clock)
+always @(posedge clock or posedge clear)
 begin
     if(r_enable)
         data_out <= data_in;
     if (clear)
         data_out <= 0;
 end
-always @(posedge clear)
-begin
-data_out <= 0;
-end
+
 
 endmodule
 
@@ -88,17 +79,14 @@ input             clear;
 input             data_in;
 output reg        data_out;
 
-always @(posedge clock)
+always @(posedge clock or posedge clear)
 begin
     if(r_enable)
         data_out <= data_in;
     if (clear)
         data_out <= 0;
 end
-always @(posedge clear)
-begin
-data_out <= 0;
-end
+
 endmodule
 
 module cpu(
