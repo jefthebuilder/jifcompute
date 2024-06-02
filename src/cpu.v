0e8b38711e5984ebb956f0ca2968836a45417202
jefthebuilder <jefgamecoffe@gmail.com>
2024-06-02 21:20:08 +0200

    update files

--- src/cpu.v
+++ src/cpu.v
@@ -6,7 +6,7 @@ input             clear;
 input      [31:0] data_in;
 output reg [31:0] data_out;
 
-always @(posedge clock)
+always @(posedge (clock | clear))
 begin
     if(r_enable)
         data_out <= data_in;
@@ -23,7 +23,7 @@ input             clear;
 input      [31:0] data_in;
 output reg [31:0] data_out;
 
-always @(posedge clock)
+always @(posedge (clock | clear))
 begin
     if(r_enable)
         data_out <= data_in;
@@ -42,7 +42,7 @@ input             clear;
 input      [4:0] data_in;
 output reg [4:0] data_out;
 
-always @(posedge clock)
+always @(posedge (clock | clear))
 begin
     if(r_enable)
         data_out <= data_in;
@@ -60,7 +60,7 @@ input             clear;
 input    [2:0]         data_in;
 output reg   [2:0]     data_out;
 
-always @(posedge clock)
+always @(posedge (clock | clear))
 begin
     if(r_enable)
         data_out <= data_in;
@@ -79,7 +79,7 @@ input             clear;
 input             data_in;
 output reg        data_out;
 
-always @(posedge clock)
+always @(posedge (clock | clear))
 begin
     if(r_enable)
         data_out <= data_in;
