82fe48d8761ebc73b71e7df01994d48e76bb47ec
jefthebuilder <162872160+jefthebuilder@users.noreply.github.com>
2024-06-05 10:08:08 +0200

    Update transceiver.v

--- src/transceiver.v
+++ src/transceiver.v
@@ -3,60 +3,65 @@ module transceiver(
     input [7:0] data,
     input reset,
     input inbit,
-    output done,
+    output reg done,
     output [7:0] dataout,
-    output outbit,
+    output reg outbit,
 );
     reg [3:0] index;
     reg [7:0] data2;
+    assign dataout = data2;
     always@(negedge clock)
         begin
+              if ( reset) begin
+              done <= 0;
+              index <= 0;
+              if ( ~done) begin
 
                 case (index)
                     0: begin
-                        assign outbit = data[0];
-                        data2[0] = inbit;
-                        assign index = 1;
+                        outbit <= data[0];
+                        data2[0] <= inbit;
+                        index <= 1;
                     end
                     1: begin
-                        assign outbit = data[1];
-                        data2[1] = inbit;
-                        assign index = 2;
+                        outbit <= data[1];
+                        data2[1] <= inbit;
+                        index <= 2;
                     end
                     2: begin
-                        assign outbit = data[2];
-                        data2[2] = inbit;
-                        assign index = 3;
+                        outbit <= data[2];
+                        data2[2] <= inbit;
+                        index <= 3;
                     end
                     3: begin
-                        assign outbit = data[3];
-                        data2[3] = inbit;
-                        assign index = 4;
+                        outbit <= data[3];
+                        data2[3] <= inbit;
+                        index <= 4;
                     end
                     4: begin
-                        assign outbit = data[0];
-                        data2[4] = inbit;
-                        assign index = 5;
+                        outbit <= data[0];
+                        data2[4] <= inbit;
+                        index <= 5;
                     end
                     5: begin
-                        assign outbit = data[0];
-                        data2[5] = inbit;
-                        assign index = 6;
+                        outbit <= data[0];
+                        data2[5] <= inbit;
+                        index <= 6;
                     end
                     6: begin
-                        assign outbit = data[0];
-                        data2[6] = inbit;
-                        assign index = 7;
+                        outbit <= data[0];
+                        data2[6] <= inbit;
+                        index <= 7;
                     end
                     7: begin
-                        assign outbit = data[0];
-                        data2[7] = inbit;
-                        assign done = 1;
-                        assign index = 0;
+                        outbit <= data[0];
+                        data2[7] <= inbit;
+                        done <= 1;
+                        index <= 0;
                     end
-
+               
                 endcase
-
+             end
         end
 
 
